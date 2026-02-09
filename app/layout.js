89115c86489a0dffb7a2117b6414c6cc5e7b8d87
blernix <killian.lecrut@gmail.com>
2026-02-09 16:09:46 +0100

    balise fb

--- app/layout.js
+++ app/layout.js
@@ -174,15 +174,42 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-WXK2NJDX');`
           }}
         />
-        {/* End Google Tag Manager */}
 
-        {/* Schema.org JSON-LD */}
+        {/* META PIXEL CODE */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '895827903143970');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* END META PIXEL CODE */}
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
       <body className={`${inter.variable} antialiased bg-white text-gray-900`}>
+        {/* Noscript pour Facebook (optionnel mais recommandé) */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=895827903143970&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
@@ -192,11 +219,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
 
-        {/* Système de navigation smooth avec fade */}
         <SmoothAnchor />
-
         {children}
       </body>
     </html>
--- next.config.mjs
+++ next.config.mjs
@@ -16,37 +16,37 @@ const nextConfig = {
         headers: [
           {
             key: 'X-Frame-Options',
-            value: 'DENY', // Empêche l'affichage dans une iframe
+            value: 'DENY',
           },
           {
             key: 'X-Content-Type-Options',
-            value: 'nosniff', // Force le respect du Content-Type
+            value: 'nosniff',
           },
           {
             key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin', // Limite les infos envoyées aux sites externes
+            value: 'strict-origin-when-cross-origin',
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()', // Désactive caméra/micro/GPS
+            value: 'camera=(), microphone=(), geolocation=()',
           },
           {
             key: 'X-DNS-Prefetch-Control',
-            value: 'on', // Active le prefetch DNS pour meilleures perfs
+            value: 'on',
           },
           {
             key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains', // Force HTTPS (une fois en prod)
+            value: 'max-age=31536000; includeSubDomains',
           },
           {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://api.emailjs.com https://www.google-analytics.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://api.emailjs.com https://www.google-analytics.com https://connect.facebook.net",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https:",
               "font-src 'self' data:",
-              "connect-src 'self' https://api.emailjs.com https://www.google-analytics.com https://*.google-analytics.com https://www.googletagmanager.com",
+              "connect-src 'self' https://api.emailjs.com https://www.google-analytics.com https://*.google-analytics.com https://www.googletagmanager.com https://www.facebook.com",
               "frame-ancestors 'none'",
               "base-uri 'self'",
               "form-action 'self'",
