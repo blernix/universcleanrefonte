daa702bfbdbce3e55fad234426a139b4a533e898
blernix <killian.lecrut@gmail.com>
2026-04-01 18:58:26 +0200

    service affichage ordre

--- app/services/[slug]/ServicePageClient.jsx
+++ app/services/[slug]/ServicePageClient.jsx
@@ -29,8 +29,8 @@ export default function ServicePageClient({ service }) {
   const [showRedirectModal, setShowRedirectModal] = useState(false);
   const [selectedFormula, setSelectedFormula] = useState(service.formulas ? 0 : null);
   const [selectedVehicleClass, setSelectedVehicleClass] = useState('classe1');
-  const [selectedCanapeSize, setSelectedCanapeSize] = useState('2-3places');
-  const [selectedMatelasSize, setSelectedMatelasSize] = useState('1place');
+  const [selectedCanapeSize, setSelectedCanapeSize] = useState('1places');
+  const [selectedMatelasSize, setSelectedMatelasSize] = useState('enfant');
 
   const handleRedirectToBooking = () => {
     setShowRedirectModal(false);
