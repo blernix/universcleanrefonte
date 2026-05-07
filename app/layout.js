05b01c72e98784f821326d83fb1ba387f43cff6d
blernix <killian.lecrut@gmail.com>
2026-05-07 13:20:35 +0200

    balise google retour

--- app/layout.js
+++ app/layout.js
@@ -171,7 +171,7 @@ export default function RootLayout({ children }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WXK2NJDX');`
+})(window,document,'script','dataLayer','G-H0LEEK7REK');`
           }}
         />
 
